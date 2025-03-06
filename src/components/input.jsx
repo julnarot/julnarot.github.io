adf3339fcb81519ca087fccdceb44a35b442f802
@Julnarot <rauljhonatan.tola@gmail.com>
2025-03-06 18:23:18 -0500

    feat: adding input readonly mode for use

---
+++ src/components/input.jsx
@@ -0,0 +1,8 @@
+export default function Input({ label, value }) {
+    const [firstCharLab, ...rest] = label ? label.split("") : "label";
+    return (
+        <div className="flex gap-2">
+            <label htmlFor="sal"><span className="text-yellow-300">{firstCharLab.toUpperCase()}</span>{rest.join("")}:</label><input type="text" name="" id="sal" value={value} className="bg-blue-600 text-white px-2 focus:outline-none" readOnly />
+        </div>
+    )
+}
