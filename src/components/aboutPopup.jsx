f7d50521414428c1cf7a277ce0a91239f8ea9983
@Julnarot <rauljhonatan.tola@gmail.com>
2025-03-12 22:34:38 -0500

    wip: update content of about

--- src/components/aboutPopup.jsx
+++ src/components/aboutPopup.jsx
@@ -1,29 +1,44 @@
-export default function AboutPopup({onClose}) {
-return (
-    <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+import Button from "./button";
+
+export default function AboutPopup({ onClose }) {
     
-          <div className="fixed inset-0 " aria-hidden="true"></div>
-    
-          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-              <div className="relative transform overflow-hidden  bg-gray-400 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-4xl  shadow-[16px_23px_0px_-9px_rgba(0,_0,_0,_1)]">
-    
-    
-                <div className="flex-1  p-1 pt-2 text-white flex flex-col md:flex-row w-full h-full overflow-auto">
-    
-                  <div className="border-4 border-double border-white p-2  relative h-full w-full ">
-                    <div className="absolute -top-3 left-3 bg-gray-400 px-1 text-sm cursor-pointer" onClick={onClose}> [<span className="text-green-400">&#9632;</span>]</div>
-                    <div className="absolute -top-3 left-1/2 bg-gray-400 transform -translate-x-1/2  px-2 text-sm">About</div>
-    
-                    dsdadasd
-                  </div>
-    
+    return (
+        <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+
+            <div className="fixed inset-0 " aria-hidden="true"></div>
+
+            <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                    <div className="relative transform overflow-hidden  bg-gray-400 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-md  shadow-[16px_23px_0px_-9px_rgba(0,_0,_0,_1)]">
+
+
+                        <div className="flex-1  p-1 pt-2  flex flex-col md:flex-row ">
+
+                            <div className="border-4 border-double border-white p-2  relative h-full w-full ">
+                                <div className="absolute -top-3 left-1/2 bg-gray-400 transform -translate-x-1/2  px-2 text-sm text-white">About</div>
+
+                                    <div class="p-4 md:p-5 text-center">
+                                        
+                                        <h3 class="mb-3 text-lg font-normal ">Borland prev++</h3>
+                                        <p class="mb-1  ">Version 1.2.0</p>
+                                        <p class="mb-1  ">Copyright (c) 2025 by</p>
+                                        <p class="mb-5  ">@Julnarot</p>
+
+                                        <Button value="Ok" onClick={onClose}/>
+                                    </div>
+
+
+
+
+
+                            </div>
+
+                        </div>
+
+
+                    </div>
                 </div>
-    
-    
-              </div>
             </div>
-          </div>
         </div>
-)
+    )
 }
