9624733b0e7deec1f8e8581e4b6c42efb85f66de
@Julnarot <rauljhonatan.tola@gmail.com>
2025-03-14 18:25:24 -0500

    impro: defining popup constants

---
+++ src/constants/popup.js
@@ -0,0 +1,10 @@
+import AboutPopup from "../components/aboutPopup";
+
+export const SUBMENU_ACTIONS = [
+    {
+        menuTitle: 'Help',
+        subMenuTitle: 'About',
+        type: 'POPUP',
+        component: AboutPopup
+    }
+];
