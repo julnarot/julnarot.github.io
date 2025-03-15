28e5ea87ae02a280d94d6cf9093e8e546d66829e
@Julnarot <rauljhonatan.tola@gmail.com>
2025-03-14 19:03:45 -0500

    impro: menu constant was added

---
+++ src/constants/menu.js
@@ -0,0 +1,14 @@
+export const MENU_DATA = [
+    {
+        title: 'File', subMenuItems: [
+            { title: 'New', shortcut: 'F3', menu: 'File' },
+            { title: 'Open', shortcut: 'F2', menu: 'File' }
+        ]
+    },
+    { title: 'Edit', subMenuItems: [] },
+    {
+        title: 'Help', subMenuItems: [
+            { title: 'About', shortcut: 'F9', menu: 'Help' }
+        ]
+    },
+];
