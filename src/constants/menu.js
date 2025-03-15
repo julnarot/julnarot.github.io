75a7915ec480fb34e2f777dbf106f4b11e3a0980
@Julnarot <rauljhonatan.tola@gmail.com>
2025-03-14 19:48:46 -0500

    wip: add option for download pdf profile

--- src/constants/menu.js
+++ src/constants/menu.js
@@ -2,7 +2,8 @@ export const MENU_DATA = [
     {
         title: 'File', subMenuItems: [
             { title: 'New', shortcut: 'F3', menu: 'File' },
-            { title: 'Open', shortcut: 'F2', menu: 'File' }
+            { title: 'Open', shortcut: 'F2', menu: 'File' },
+            { title: 'Print', shortcut: 'F3', menu: 'File' },
         ]
     },
     { title: 'Edit', subMenuItems: [] },
