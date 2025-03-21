3f350091f5dc9e159d61f94d871949f97a219dd0
@Julnarot <rauljhonatan.tola@gmail.com>
2025-03-20 21:17:40 -0500

    refactor: timeUtils was updated

--- src/utils/timeUtils.js
+++ src/utils/timeUtils.js
@@ -1,4 +1,6 @@
-export const getDuration = (startDate, endDate) => {
+export const getDurationBetweenStrDates = (startStrDate, endStrDate) => {
+    const startDate = new Date(startStrDate);
+    const endDate = new Date(endStrDate);
     let years = endDate.getFullYear() - startDate.getFullYear();
     let months = endDate.getMonth() - startDate.getMonth();
 
@@ -8,3 +10,8 @@ export const getDuration = (startDate, endDate) => {
     }
     return years > 0 ? `${years} años y ${months} meses` : `${months} meses`;
 };
+
+export const strDateToStrMonthYear = (stringDate) => {
+    const _date = new Date(stringDate);
+    return `${_date.getMonth() + 1}/${_date.getFullYear()}`;
+}
