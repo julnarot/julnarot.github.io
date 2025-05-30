f99fb38c6583a3be8e723aa1ab29feef261efcc8
@Julnarot <rauljhonatan.tola@gmail.com>
2025-05-29 23:24:18 -0500

    feat: replace label with Label component

--- src/components/core/input.jsx
+++ src/components/core/input.jsx
@@ -1,7 +1,7 @@
 import Label from "./label";
 
 export default function Input({ label, value, className }) {
-  const concClass = `bg-blue-600 dark:bg-purple-900 text-white px-2 focus:outline-none ${className}`;
+  const concClass = `bg-blue-600 dark:bg-purple-900 text-white px-2 w-full focus:outline-none ${className}`;
   return (
     <div className="flex flex flex-col md:flex-row mb-2 mr-4">
       {label && <Label label={label} firstCharHighLighting={true} />}
